5e856724eb9aaeb6f7a464de4a3016d5f1e91a4c
agonzalez1216 <73087812+agonzalez1216@users.noreply.github.com>
2020-12-07 11:26:45 -0500

    Update ph2-solutions.sql

--- ph2-solutions.sql
+++ ph2-solutions.sql
@@ -24,4 +24,15 @@ USE sakila;
 6c.select title, count(actor_id) from film join film_actor using(film_id) group by title;
 6d.select count(*) from inventory join film using(film_id) where title = 'Hunchback Impossible';
 6e.select sum(amount), first_name, last_name from payment join customer using(customer_id) group by customer_id order by last_name;
-7a.
+7a.select * from film join language using(language_id) where name = 'English' and title in (select title from film where(title like 'K%' or title like 'Q%'));
+7b.select first_name, last_name from actor join film_actor using(actor_id) where film_id = (select film_id from film where title = 'alone trip');
+7c.select first_name, last_name, email from customer join address using(address_id) join city using(city_id) join country using(country_id) where country='Canada';
+7d.select * from film join film_category using(film_id) join category using(category_id) where name = 'Family';
+7f.select * from sales_by_store;
+7e.select title, count(film_id) from film join inventory using(film_id) join rental using(inventory_id) group by title order by count(film_id) desc;
+7g.select store_id, city, country from store join address using(address_id) join city using(city_id) join country using(country_id);
+7h.select name, sum(amount) from category join film_category using(category_id) join inventory using(film_id) join rental using(inventory_id) join payment using(customer_id) group by name order by sum(amount) desc limit 5;
+8a.create view top_5_genres_gross_rev as select name, sum(amount) from category join film_category using(category_id) join inventory using(film_id) join rental using(inventory_id) join payment using(customer_id) group by name order by sum(amount) desc limit 5;
+8b.select * from top_5_genres_gross_rev;
+8c.drop view top_5_genres_gross_rev;
+                                                                                                      
